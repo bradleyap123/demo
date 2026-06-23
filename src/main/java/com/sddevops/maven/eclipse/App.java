319a69e34c5cb95dcdb34072202d0e6cc2043aad
bradleyap123 <bradley.yap@hotmail.com>
2026-06-23 20:53:03 +0800

    (feat) Add test line

--- src/main/java/com/sddevops/maven/eclipse/App.java
+++ src/main/java/com/sddevops/maven/eclipse/App.java
@@ -1,10 +1,8 @@
 package com.sddevops.maven.eclipse;
 
-/**
- * Hello world!
- */
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("bradley test!");
+        System.out.println("new line");
     }
 }
